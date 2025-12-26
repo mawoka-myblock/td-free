c81e5dadc08dcb9b69462e1eaa92c5d701567957
Mawoka <git@mawoka.eu>
2025-12-26 19:55:45 +0100

    Fix LED dimming down after measurement

--- src/helpers/readings.rs
+++ src/helpers/readings.rs
@@ -336,7 +336,10 @@ pub async fn read_data_with_buffer(
         let rgb_buf = rgb_d.rgb_buffers.lock().unwrap();
         (lux_buf.len(), rgb_buf.0.len())
     };
-
+    {
+        let mut led = led_light.lock().unwrap();
+        led.set_duty_cycle_percent(25).unwrap();
+    }
     log::info!(
         "Reading: {:.2}, RGB: {} (medians from {} lux, {} RGB samples, confidence: {}), Raw RGB: ({},{},{}), Final RGB: ({},{},{}) - Baseline: {:.2}, Lux: {}, Clear: {}",
         adjusted_td_value,
